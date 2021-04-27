7b77872fb44b17c4ae89afbe8bb8bd967b227254
Christian Häussler <c-n-i@web.de>
2021-04-27 22:15:44 +0200

    Update to Version 1.3.7
    Signed-off-by: Christian Häussler <c-n-i@web.de>

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker build . --tag cniweb/cpuminer-multi:latest
-#docker push cniweb/cpuminer-multi:latest
+docker build . --tag cniweb/cpuminer-multi:1.3.7
+docker tag cniweb/cpuminer-multi:1.3.7 ghcr.io/cniweb/cpuminer-multi:1.3.7
+docker tag cniweb/cpuminer-multi:1.3.7 ghcr.io/cniweb/cpuminer-multi:latest
+docker push ghcr.io/cniweb/cpuminer-multi:1.3.7
+docker push ghcr.io/cniweb/cpuminer-multi:latest
